1da0cbe562e9c884ed43f36b8d9348e482a6da3f
Rodrigo <rod_igomail@hotmail.com>
2023-05-08 12:16:38 -0300

    feito o _document.tsx e instalado o pacote @emotion/server

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,13 +1,45 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import React from "react";
+import Document, { Html, Head, Main, NextScript, DocumentContext } from "next/document";
+import createEmotionServer from "@emotion/server/create-instance";
+import createEmotionCache from "../logica/servicos/EmotionCache";
 
-export default function Document() {
-    return (
-        <Html lang="en">
-            <Head />
-            <body>
-                <Main />
-                <NextScript />
-            </body>
-        </Html>
-    );
+export default class QualquerNome extends Document {
+    static async getInitialProps(contexto: DocumentContext) {
+        const renderizacaoPaginaOriginal = contexto.renderPage;
+        const cache = createEmotionCache();
+        const { extractCriticalToChunks } = createEmotionServer(cache);
+        contexto.renderPage = () =>
+            renderizacaoPaginaOriginal({
+                enhanceApp: (Aplicacao: any) =>
+                    function EnhanceApp(propriedades) {
+                        return <Aplicacao emotionCache={cache} {...propriedades} />;
+                    },
+            });
+        const propriedadesIniciais = await Document.getInitialProps(contexto);
+        const emotionStyles = extractCriticalToChunks(propriedadesIniciais.html);
+        const emotionStyleTags = emotionStyles.styles.map((estilo) => (
+            <style
+                data-emotion={`${estilo.key} ${estilo.ids.join(" ")}`}
+                key={estilo.key}
+                dangerouslySetInnerHTML={{ __html: estilo.css }}
+            />
+        ));
+
+        return {
+            ...propriedadesIniciais,
+            emotionStyleTags,
+        };
+    }
+
+    render(): JSX.Element {
+        return (
+            <Html lang="pt-BR">
+                <Head>{(this.props as any).emotionStyleTags}</Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
+    }
 }
