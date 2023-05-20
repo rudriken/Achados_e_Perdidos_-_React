fdcedbe0aa26f462c98be1fd4d922e1ebb52facb
Rodrigo <rod_igomail@hotmail.com>
2023-05-20 09:31:45 -0300

    alterei a interface no Hook useLogin para uma interface mais específica

--- src/logica/ganchos/pages/useLogin.ts
+++ src/logica/ganchos/pages/useLogin.ts
@@ -3,15 +3,15 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { ServicoLogin } from "@/logica/servicos/ServicoLogin";
 import { ServicoEstruturaFormulario } from "@/logica/servicos/ServicoEstruturaFormulario";
-import { FrontEndUsuarioInterface } from "../../interfaces/FrontEndInterfaces";
+import { FrontEndLoginInterface } from "../../interfaces/FrontEndInterfaces";
 
 export default function useLogin() {
-    const formularioMetodosLogin = useForm<FrontEndUsuarioInterface>({
+    const formularioMetodosLogin = useForm<FrontEndLoginInterface>({
             resolver: yupResolver(ServicoEstruturaFormulario.login()),
         }),
         [erro, alterarErro] = useState(false);
 
-    async function logar(credenciais: FrontEndUsuarioInterface): Promise<void> {
+    async function logar(credenciais: FrontEndLoginInterface): Promise<void> {
         const sucesso = await ServicoLogin.entrar(credenciais);
         if (sucesso) {
             alterarErro(false);
