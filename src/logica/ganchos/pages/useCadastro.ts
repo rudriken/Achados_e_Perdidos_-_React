4a3749abf279e80e9f3ebf27abd8f77ab86fd43d
Rodrigo <rod_igomail@hotmail.com>
2023-06-01 10:11:16 -0300

    refatorando uma interface

--- src/logica/ganchos/pages/useCadastro.ts
+++ src/logica/ganchos/pages/useCadastro.ts
@@ -3,18 +3,17 @@ import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { ServicoApi } from "@/logica/servicos/ServicoApi";
 import { ServicoEstruturaFormulario } from "@/logica/servicos/ServicoEstruturaFormulario";
-import { FrontEndLocalUsuarioInterface } from "@/logica/interfaces/FrontEndInterfaces";
 import { LocalUsuarioInterface } from "@/logica/interfaces/interfaces";
 
 export default function useCadastro() {
-    const formularioMetodosCadastro = useForm<FrontEndLocalUsuarioInterface>({
+    const formularioMetodosCadastro = useForm<LocalUsuarioInterface>({
             resolver: yupResolver(ServicoEstruturaFormulario.cadastro()),
         }),
         [mensagem, alterarMensagem] = useState(false);
 
-    async function cadastrar(dados: FrontEndLocalUsuarioInterface): Promise<void> {
+    async function cadastrar(dados: LocalUsuarioInterface): Promise<void> {
         try {
-            await ServicoAPI.post<BackEndLocalUsuarioInterface>("/api/locais", dados);
+            await ServicoApi.post<LocalUsuarioInterface>("/api/locais", dados);
             alterarMensagem(true);
         } catch (erro) {
             return;
