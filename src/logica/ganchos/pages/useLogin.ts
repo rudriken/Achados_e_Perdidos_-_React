148d97c8c85b834b713163dd5ee454b59216c2ec
Rodrigo <rod_igomail@hotmail.com>
2023-06-13 18:09:00 -0300

    ao logar, agora o usuário é direcionado para a plataforma

--- src/logica/ganchos/pages/useLogin.ts
+++ src/logica/ganchos/pages/useLogin.ts
@@ -1,20 +1,25 @@
-import { useState } from "react";
+import { useContext, useState } from "react";
 import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { ServicoLogin } from "@/logica/servicos/ServicoLogin";
 import { ServicoEstruturaFormulario } from "@/logica/servicos/ServicoEstruturaFormulario";
 import { LoginInterface } from "@/logica/interfaces/interfaces";
+import { ContextoDoLocalUsuario } from "@/logica/contextos/ContextoDoLocalUsuario";
 
 export default function useLogin() {
     const formularioMetodosLogin = useForm<LoginInterface>({
             resolver: yupResolver(ServicoEstruturaFormulario.login()),
         }),
         [erro, alterarErro] = useState(false);
+    const { despachoDoLocalUsuario } = useContext(ContextoDoLocalUsuario);
 
     async function logar(credenciais: LoginInterface): Promise<void> {
         const sucesso = await ServicoLogin.entrar(credenciais);
         if (sucesso) {
-            window.location.reload();
+            const localUsuario = await ServicoLogin.informacoesDoLocalUsuario();
+            if (localUsuario) {
+                despachoDoLocalUsuario({ tipo: "ATUALIZAR_DADOS", carga: localUsuario });
+            }
         } else {
             alterarErro(true);
         }
